5e6af4c8853605b283a90e86f29c2757dc75f708
Jamel Hendricks <jamel@ibm.com>
2021-10-02 17:27:09 -0500

    refactor base code, add typescript/prettier/eslinting packages & rules

---
+++ empathylab/src/App.tsx
@@ -0,0 +1,13 @@
+import './styles/index.scss';
+import React from 'react';
+import Navigation from './components/Navigation';
+
+function App(): JSX.Element {
+	return (
+		<>
+			<Navigation />
+		</>
+	);
+}
+
+export default App;
